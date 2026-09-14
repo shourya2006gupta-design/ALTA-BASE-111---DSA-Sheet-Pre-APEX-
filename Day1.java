556cd41bcd72e01a99317fa85395263d7fbc742b
shourya2006gupta-design <shourya2006gupta@gmail.com>
2026-09-14 19:40:50 +0530

    added day 1 code
    Write a program that takes the number of days a person has lived and estimates their age using integer division by 365, printing both the whole years and the remainder days. Input: 7305  Output: 20 years, 5 days

---
+++ Day1.java
@@ -0,0 +1,12 @@
+// Write a program that takes the number of days a person has lived and estimates their age using integer division by 365, printing both the whole years and the remainder days. Input: 7305  Output: 20 years, 5 days
+
+import java.util.*;
+
+class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int daysLived = sc.nextInt(); // taking input
+
+        System.out.println(daysLived / 365 + " years, " + daysLived % 365 + " days"); // returning result
+    }
+}
