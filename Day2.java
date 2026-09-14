07798980111ff8859c4cb5c013956760b31c512f
shourya2006gupta-design <shourya2006gupta@gmail.com>
2026-09-14 19:49:41 +0530

    code for day 2
    Write a program to swap the values of two variables using a third (temporary) variable.Input: a = 5, b = 10  Output: a = 10, b = 5

---
+++ Day2.java
@@ -0,0 +1,23 @@
+//QUESTION
+// Write a program to swap the values of two variables using a third (temporary) variable.Input: a = 5, b = 10  Output: a = 10, b = 5
+
+import java.util.*;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(); // taking input for variable 1
+        int b = sc.nextInt(); // taking input for variable 2
+
+        int c; // making a temporary variable
+
+        // swapping
+        c = b;
+        b = a;
+        a = c;
+
+        // returning result
+        System.out.println("Swapped 1st variable is " + a + "\nSwapped 2nd variable is " + b);
+    }
+}
